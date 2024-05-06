583bd7e49927ed149b15eab24dc3664ca36dc6d4
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-05-06 14:45:46 +0530

    changes

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Search from './Search'
+import Movies from './Movies'
+const Home = () => {
+  // const name = useContext(AppContext)
+  // const {movie} = useGlobalContext()
+  return (
+    <>
+      <Search />
+      <Movies />
+    </>
+  )
+}
+
+export default Home
